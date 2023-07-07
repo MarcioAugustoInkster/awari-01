3a079dc9e5f99acbfc07f9fdeaca64785720a0f4
Marcio Augusto <marcio_augusto_09@hotmail.com>
2023-07-07 15:04:23 -0300

    single project exercices added

---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+  return (
+    <>
+      <h1>404 - Página não encontrada</h1>
+      <Link to="/">Voltar para Página Inicial</Link>
+    </>
+  );
+};
+
+export default Error;
